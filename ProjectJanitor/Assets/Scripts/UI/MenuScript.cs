90105b5f2a523dd4473a7b62aef64818f87db829
S4D3H <fleabassmaster@hotmail.com>
2016-01-19 14:39:45 +0100

    Menus

--- ProjectJanitor/Assets/Scripts/UI/MenuScript.cs
+++ ProjectJanitor/Assets/Scripts/UI/MenuScript.cs
@@ -3,35 +3,57 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class MenuScript : MonoBehaviour {
+public class MenuScript : MonoBehaviour
+{
 
     public Canvas quitMenu;
     public Button startGame;
     public Button exitGame;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         quitMenu = quitMenu.GetComponent<Canvas>();
         startGame = startGame.GetComponent<Button>();
         exitGame = exitGame.GetComponent<Button>();
         quitMenu.enabled = false;
     }
 
-    public void ExitPress()
+    void Update()
+    {
+        EscapeTracker();
+    }
+
+    public void EscapeTracker()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (quitMenu.enabled == true)
+            {
+                NoButtonPress();
+            }
+            else
+            {
+                QuitGamePress();
+            }
+        }
+    }
+
+    public void QuitGamePress()
     {
         quitMenu.enabled = true;
         startGame.enabled = false;
         exitGame.enabled = false;
     }
 
-    public void NoPress()
+    public void NoButtonPress()
     {
         quitMenu.enabled = false;
         startGame.enabled = true;
         exitGame.enabled = true;
     }
 
-    public void StartLevel()
+    public void NewGamePress()
     {
         //Application.LoadLevel("scn_CharSelection");
         SceneManager.LoadScene("scn_CharSelection");
@@ -40,12 +62,17 @@ public class MenuScript : MonoBehaviour {
     public void LoadMenu()
     {
         //Application.LoadLevel("scn_LoadMenu");
-        SceneManager.LoadScene("scn_LoadMenu");
+        SceneManager.LoadScene("scn_LoadGame");
     }
 
-    public void ExitGame()
+    public void YesButtonPress()
     {
         Application.Quit();
+    }
+
+    public void BackButtonPress()
+    {
+        SceneManager.LoadScene("scn_MainMenu");
     }
 
 }
